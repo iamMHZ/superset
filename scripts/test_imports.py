460b0af7bfe217098621a2e74e605bbee8b3c8bb
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-05-09 10:27:26 -0700

    more fixage

--- scripts/test_imports.py
+++ scripts/test_imports.py
@@ -1,31 +1,50 @@
 import os
+import re
+import subprocess
+from typing import List
 
-# mypy: ignore-errors
+EXCLUDE_FILE_PATTERNS: List[str] = [
+    "migrations/",
+]
 
-# pylint: skip-file
-# pylint: disable-all
+
+def test_module_import(file_path: str) -> None:
+    """Test if a module can be imported independently"""
+    module_path = file_path.replace(".py", "").replace("/", ".")
+    # if module_path.endswith("__init__"):
+    #    module_path = module_path.replace(".__init__", "")
+    splitted = ["superset"] + module_path.split(".")
+    from_part = ".".join(splitted[:-1])
+    import_part = splitted[-1]
+    import_statement = f"from {from_part} import {import_part}"
+    try:
+        subprocess.run(
+            ["python", "-c", import_statement],
+            check=True,
+        )
+        # print(f"✅ {import_statement}")
+    except subprocess.CalledProcessError as e:
+        print(f"❌ {import_statement}")
+        if e.stderr:
+            print(f"ERROR: {e.stderr}")
 
 
-def generate_import_statements(root_dir):
-    import_statements = set()
-    for dirpath, _, filenames in os.walk(root_dir):
-        for filename in filenames:
-            if filename.endswith(".py"):
-                module_path = os.path.relpath(os.path.join(dirpath, filename), root_dir)
-                module_name = os.path.splitext(module_path.replace(os.path.sep, "."))[0]
-                splitted = module_name.split(".")
-                if splitted[-1] == "__init__":
-                    splitted.pop()
-                if len(splitted) > 1 and "migration" not in module_path:
-                    package = ".".join(splitted[:-1])
-                    import_statements.add(
-                        f"from superset.{package} import {splitted[-1]}"
-                    )
-    return sorted(import_statements)
+def test_import(package_path: str) -> None:
+    """Test importability of all modules within a package"""
+    for root, dirs, files in os.walk(package_path):
+        for file in files:
+            filepath = os.path.normpath(os.path.join(root, file))
+            relative_path = os.path.relpath(filepath, package_path)
+            if file.endswith(".py") and all(
+                not re.match(pattern, relative_path)
+                for pattern in EXCLUDE_FILE_PATTERNS
+            ):
+                test_module_import(relative_path)
 
 
 if __name__ == "__main__":
-    root_dir = "superset"
-    import_statements = generate_import_statements(root_dir)
-    for statement in import_statements:
-        print(statement)
+    package_path = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "../superset")
+    )
+    print(f"Processing package located at: {package_path}")
+    test_import(package_path)
